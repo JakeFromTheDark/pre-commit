780b021431b0d59552f8be0a169da6d3eb9c3d58
Kamil Jakubovic <kamil.jakubovic@sap.com>
2020-07-25 14:47:11 +0200

    ansible-playbook

--- hooks/ansible-playbook.sh
+++ hooks/ansible-playbook.sh
@@ -4,14 +4,19 @@ declare playbook
 declare output=
 declare retcode=0
 
+declare color_red=$'\x1b[31m'
+declare color_yellow=$'\x1b[1;33m'
+declare color_normal=$'\x1b[0m'
+
 for playbook; do
   output="$(ansible-playbook -i localhost, --syntax-check -- "${playbook}" 2>&1)" && continue
 
   # failed
-  retcode=$?
-  echo "Playbook: ${playbook}"
+  retcode="$?"
+  echo    "${color_yellow}${playbook}:"
+  echo -n "${color_red}"
   grep -v '^ \[WARNING\]: Could not match supplied host pattern, ignoring: ' <<<"${output}"
-  echo
+  echo -e "${color_normal}"
 done
 
 exit "${retcode}"
